c28072bbfe66aa009a01bc76209ccd2a6e003e4d
FilipeSantos22 <131908875+FilipeSantos22@users.noreply.github.com>
2025-11-13 15:27:02 -0300

    Increase logo image height in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
             <a href="/" className="flex items-center">
-              <img src={logo} alt="WinClick" className="h-11" />
+              <img src={logo} alt="WinClick" className="h-12" />
             </a>
           </div>
           
