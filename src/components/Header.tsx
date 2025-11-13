3caedb607ea4389471a3f4c89f458aeaef1b4170
FilipeSantos22 <131908875+FilipeSantos22@users.noreply.github.com>
2025-11-13 15:10:42 -0300

    Update logo image in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Instagram } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo-fundo-preto-removebg-preview.png";
 
 const Header = () => {
   return (
